a0d19b7849e72c2aabfddb5ffb122f8f1b0d0c11
Alex Seubert <alexseubert@gmail.com>
2023-12-04 21:52:15 -0600

    Day 4 Part 2

--- lib/day04.ex
+++ lib/day04.ex
@@ -24,8 +24,48 @@ defmodule Day04.Part1 do
   end
 end
 
+defmodule Day04.Card do
+  defstruct [wins: 0, copies: 1]
+end
+
 defmodule Day04.Part2 do
+  require IEx
   def solve(input) do
+    card_wins = input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_line/1)
+
+    calculated_wins = Enum.reduce(card_wins, Map.new(card_wins), fn {card_number, card}, acc ->
+      case card.wins do
+        0 -> acc
+        _ -> add_wins(acc, card_number, card.wins)
+      end
+    end)
+
+    Enum.reduce(calculated_wins, 0, fn {card_number, card}, acc ->
+      acc + card.copies
+    end)
+  end
+
+  def add_wins(cards, card_number, wins) do
+    Enum.reduce(card_number + 1 .. card_number + wins, cards, fn card_number_to_change, acc ->
+      calculating_card = Map.get(cards, card_number)
+      card_to_change = Map.get(cards, card_number_to_change)
+      case card_to_change do
+        nil -> acc
+        _ -> Map.put(acc, card_number_to_change, %{card_to_change | copies: card_to_change.copies + calculating_card.copies})
+      end
+    end)
+  end
+
+  def parse_line(line) do
+    parts = String.split(line, ":")
+    card_number = Regex.run(~r/\d+/, List.first(parts)) |> List.flatten() |> List.first() |> String.to_integer()
+    number_parts = String.split(List.last(parts), ~r/\|/) |> List.flatten()
+    winning_numbers = Regex.scan(~r/\d+/, List.first(number_parts)) |> MapSet.new()
+    my_numbers = Regex.scan(~r/\d+/, List.last(number_parts)) |> MapSet.new()
+    wins = MapSet.intersection(winning_numbers, my_numbers) |> MapSet.size()
+    {card_number, %Day04.Card{wins: wins}}
   end
 end
 
